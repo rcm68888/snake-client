527e882e3b162abfca574d68b7e3f9e34b5a564a
Rcm <rcm68888@gmail.com>
2023-12-17 06:01:43 -0700

    2nd Commit

---
+++ client.js
@@ -0,0 +1,30 @@
+const { IP, PORT } = require('./constants');
+const net = require('net');
+
+/**
+ * Establishes connection w/ game server
+ */
+const connect = function(){
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
+  });
+
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+    conn.write('Name: RcM');
+  })
+
+  conn.on('data', (data) => {
+    console.log(data)
+  })
+
+  return conn;
+}
+
+module.exports = {
+  connect
+}
---
+++ input.js
@@ -0,0 +1,41 @@
+// Stores the active TCP connection object.
+let connection;
+
+const handleUserInput = (key) => {
+  // \u0047 => g
+  if(key === '\u0003') {    
+    process.exit();
+  } else if (key === '\u0077') {
+    // w
+    connection.write('Move: up')
+  } else if (key === '\u0061') {
+    // a
+    connection.write('Move: left')
+  } else if (key === '\u0073') {
+    // s
+    connection.write('Move: down')
+  } else if (key === '\u0064') {
+    // d
+    connection.write('Move: right')
+  }
+
+  if(key === 'f') {
+    connection.write('Say: f in chat');
+  } else if (key === 'g') {
+    connection.write('Say: ggg');
+  } else if (key === 'l') {
+    connection.write('Say: glll!!!!!')
+  }
+}
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+module.exports = {
+  setupInput
+}
